bc0e68631edc23458c908f3d52590fb40623b8ad
mralifakbar <alif.akbarkartadinata2049@students.unila.ac.id>
2021-05-02 13:04:29 +0700

    Add reverse function

--- findwords.cpp
+++ findwords.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 const int cols = 16, rows = 15;
 
- char words[rows][cols] = {	"tgbwwinterwsesn",
+char words[rows][cols] = {	"tgbwwinterwsesn",
                             "aaunttmmhfoodnb",
                             "jlwcqldzmpmvdmr",
                             "asagmquwvvbsohi",
@@ -50,4 +50,12 @@ char *getWordVertical(int columnIndex){
 		verticalWord[i] = words[i][columnIndex];
 	}
 	return verticalWord;
+}
+
+char *reverse(char *strToReverse){
+	char *reverseResult = (char *) malloc strlen(strToReverse);
+	for(int i = 0; i < strlen(strToReverse); i++){
+		reverseResult[i] = strToReverse[strlen(strToReverse) - 1 - i];
+	}
+	return reverseResult;
 }
